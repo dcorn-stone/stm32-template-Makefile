bde78d136ccb8f4c013386cbba18814f52124a25
dcorn-stone <gayi_chen@outlook.com>
2025-05-04 23:49:14 +0800

    First approach of converting the project to SPL

---
+++ Src/main.c
@@ -0,0 +1,22 @@
+// #include "stm32f10x.h"
+// #include "stm32f10x_gpio.h"
+// #include "stm32f10x_rcc.h"
+
+int main(void){
+    // // Enable GPIOA clock
+    // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    // // Configure PA0 as input floating
+    // GPIO_InitTypeDef GPIO_InitStructure;
+    // GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    // GPIO_InitStructure.GPIO_Mode = GPIO_Mode_PP;
+    // GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    // while(1){
+    //     GPIO_SetBit(GPIOC, GPIO_Pin_13); 
+
+    //     GPIO_ResetBit(GPIOC, GPIO_Pin_13);
+
+    // }
+
+}
